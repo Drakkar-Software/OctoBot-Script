8d85d70339fc1bc9817a1b0477f21c681810a620
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-12-19 23:49:38 +0100

    [Quality] factorize code

--- octobot_pro/model/backtest_plot.py
+++ octobot_pro/model/backtest_plot.py
@@ -2,10 +2,11 @@ import webbrowser
 import jinja2
 import json
 
-import octobot_pro.resources as resources
 import octobot_commons.constants as commons_constants
 import octobot_commons.display as display
 import octobot_commons.logging as logging
+import octobot.api as octobot_api
+import octobot_pro.resources as resources
 import octobot_pro.internal.backtester_trading_mode as backtester_trading_mode
 
 
@@ -51,15 +52,15 @@ class BacktestPlot:
         symbols = []
         time_frames = []
         exchanges = []
-        for exchange, available_symbols in self.backtest_result.independent_backtesting.octobot_backtesting. \
-             symbols_to_create_exchange_classes.items():
+        for exchange, available_symbols in octobot_api.get_independent_backtesting_symbols_by_exchanges(
+                self.backtest_result.independent_backtesting
+        ).items():
             exchanges.append(exchange)
             for symbol in available_symbols:
                 symbol = str(symbol)
                 symbols.append(symbol)
-                for time_frame in self.backtest_result.independent_backtesting.octobot_backtesting.backtesting_config[
-                    commons_constants.CONFIG_TIME_FRAME
-                ]:
+                for time_frame in octobot_api.get_independent_backtesting_config(
+                        self.backtest_result.independent_backtesting)[commons_constants.CONFIG_TIME_FRAME]:
                     time_frames.append(time_frame.value)
                     await elements.fill_from_database(
                         trading_mode, self.run_db_identifier, exchange, symbol, time_frame.value,
--- octobot_pro/resources/__init__.py
+++ octobot_pro/resources/__init__.py
@@ -2,6 +2,7 @@ import os
 
 
 def get_report_resource_path(resource_name):
+    base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "reports")
     if resource_name:
-        return os.path.join(os.path.dirname(os.path.abspath(__file__)), "reports", resource_name)
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "reports")
+        return os.path.join(base_path, resource_name)
+    return base_path
