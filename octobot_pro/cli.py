96b685efe1f7884c191e80aa5cd2a1bb008d2412
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-12-27 19:30:10 +0100

    [Licence] add licence to files

--- octobot_pro/cli.py
+++ octobot_pro/cli.py
@@ -1,18 +1,20 @@
-#  Drakkar-Software OctoBot-Tentacles-Manager
-#  Copyright (c) Drakkar-Software, All rights reserved.
+#  This file is part of OctoBot-Pro (https://github.com/Drakkar-Software/OctoBot-Pro)
+#  Copyright (c) 2022 Drakkar-Software, All rights reserved.
 #
-#  This library is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public
-#  License as published by the Free Software Foundation; either
+#  OctoBot is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either
 #  version 3.0 of the License, or (at your option) any later version.
 #
-#  This library is distributed in the hope that it will be useful,
+#  OctoBot is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#  Lesser General Public License for more details.
+#  General Public License for more details.
 #
-#  You should have received a copy of the GNU Lesser General Public
-#  License along with this library.
+#  You should have received a copy of the GNU General Public
+#  License along with OctoBot-Pro. If not, see <https://www.gnu.org/licenses/>.
+
+
 import click
 import asyncio
 import aiohttp
