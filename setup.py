042c0251523dcae1f11c6ffd71966d786350cf0a
Herklos <herklos.dev@protonmail.com>
2023-10-09 22:56:50 +0200

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.command.install import install
 # from octobot_script import PROJECT_NAME, VERSION
 # todo figure out how not to import octobot_script.__init__.py here
 PROJECT_NAME = "OctoBot-Script"
-VERSION = "0.0.11"  # major.minor.revision
+VERSION = "0.0.13"  # major.minor.revision
 
 
 def _post_install():
