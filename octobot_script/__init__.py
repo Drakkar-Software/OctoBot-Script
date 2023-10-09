2e0a720d64e081dcc18bbbecbd2ce9eb25c4829d
Herklos <herklos.dev@protonmail.com>
2023-10-09 22:56:50 +0200

    Update version

--- octobot_script/__init__.py
+++ octobot_script/__init__.py
@@ -16,7 +16,7 @@
 
 PROJECT_NAME = "OctoBot-Script"
 AUTHOR = "Drakkar-Software"
-VERSION = "0.0.13"  # major.minor.revision
+VERSION = "0.0.13"  # major.minor.revision => don't forget to also update the setup.py version
 
 
 def _use_module_local_tentacles():
