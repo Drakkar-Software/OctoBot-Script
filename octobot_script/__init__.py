dcc76199e6c7a3a5158ea824719de1700a7e4989
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2024-05-01 21:06:12 +0200

    [Version] v0.0.17

--- octobot_script/__init__.py
+++ octobot_script/__init__.py
@@ -16,7 +16,7 @@
 
 PROJECT_NAME = "OctoBot-Script"
 AUTHOR = "Drakkar-Software"
-VERSION = "0.0.16"  # major.minor.revision => don't forget to also update the setup.py version
+VERSION = "0.0.17"  # major.minor.revision => don't forget to also update the setup.py version
 
 
 def _use_module_local_tentacles():
