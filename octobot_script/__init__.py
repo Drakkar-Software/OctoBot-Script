556d9b346e133727dd16396df66387e910145e60
Herklos <herklos@drakkar.software>
2026-02-27 10:02:20 +0100

    Update CHANGELOG
    Signed-off-by: Herklos <herklos@drakkar.software>

--- octobot_script/__init__.py
+++ octobot_script/__init__.py
@@ -16,7 +16,7 @@
 
 PROJECT_NAME = "OctoBot-Script"
 AUTHOR = "Drakkar-Software"
-VERSION = "0.0.29"  # major.minor.revision => don't forget to also update the setup.py version
+VERSION = "0.1.0"  # major.minor.revision => don't forget to also update the setup.py version
 
 
 def _use_module_local_tentacles():
